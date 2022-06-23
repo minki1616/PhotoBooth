63fa9724a33aa963b10c94d4b82b92bed4a33957
Munish Sharma <munish.s.sharma@oracle.com>
2022-06-23 22:19:01 +0530

    Reformated to generic

--- src/Driver.java
+++ src/Driver.java
@@ -2,7 +2,7 @@ import handler.Handler;
 import java.util.Map;
 import model.Picture;
 import model.User;
-import util.Logging;
+import static util.Logging.logger;
 import util.PictureMap;
 import util.UserMap;
 
@@ -13,14 +13,16 @@ public class Driver {
         PictureMap pictureMap = new PictureMap();
         UserMap userMap = new UserMap();
         Handler handler = new Handler(pictureMap, userMap);
-
-        User user1 = handler.registerUser("user1");
-        Picture picture1 = handler.postPicture("user1", "image1");
-        picture1.addLike("user1");
+        User user1 = handler.registerUser("user2","Alpha");
+        Picture picture1 = handler.postPicture("user2", "image2");
+        picture1.addLike("user2");
 
         Map<String, Picture> map =  pictureMap.getPictureMap();
-        for(String itr: map.keySet()){
+        for(String itr: map.keySet()){  // To iterate key only
             System.out.println("Picture - " + itr);
+        }
+        for(Map.Entry<String, Picture> itr: map.entrySet()){  // To iterate key and value
+            System.out.println("Picture Key: " + itr.getKey() + ", Value:"+itr.getValue()) ;
         }
     }
 }
--- src/handler/Handler.java
+++ src/handler/Handler.java
@@ -1,11 +1,9 @@
 package handler;
 
 import static util.Logging.logger;
-
 import java.util.logging.Level;
 import model.Picture;
 import model.User;
-import util.Logging;
 import util.PictureMap;
 import util.UserMap;
 
@@ -13,25 +11,24 @@ public class Handler {
     private final PictureMap pictureMap;
     private final UserMap userMap;
 
-    public Handler( PictureMap pictureMap, UserMap userMap)
-    {
+    public Handler( PictureMap pictureMap, UserMap userMap) {
         this.pictureMap=pictureMap;
         this.userMap=userMap;
     }
 
-    public User registerUser(String userID){
-        Logging.logger.log(Level.INFO, "Register user with User ID: " + userID);
-        User user1 = new User("user1", "Alpha");
-        userMap.addUser("user1", user1);
-        return user1;
+    public User registerUser(String userID, String name){
+        logger.log(Level.INFO, "Register user with User ID: " + userID);
+        User user = new User(userID, name);
+        userMap.addUser(userID, user);
+        return user;
     }
 
     public Picture postPicture(String userID, String image){
-        Logging.logger.log(Level.INFO, "Post Picture for User: " +userID + ", Image Id: "+ image);
+        logger.log(Level.INFO, "Post Picture for User: " +userID + ", Image Id: "+ image);
         User user = userMap.getUser(userID);
         Picture picture = new Picture(image, userID);
         user.addPictures(picture);
-        pictureMap.addPicture("image1", picture);
+        pictureMap.addPicture(image, picture);
         return picture;
     }
 }
